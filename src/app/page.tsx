7ec2e04ea849b8c96c80560274c65b591d0de898
Joe Fird <162990141+joesingtecs@users.noreply.github.com>
2024-10-12 08:40:33 +0300

    change init page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { createClient } from '../../utils/supabase/server';
+import { NextResponse } from 'next/server';
 
 export default async function Home() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("transactions").select().limit(3);
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  return NextResponse.json("Welcome to Nonton API");
 }
