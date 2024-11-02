79c404a079dcfffdfe018ecb52f65dec4089b1e5
Joe Fird <162990141+joesingtecs@users.noreply.github.com>
2024-11-02 02:00:49 +0200

    change privatekey to private in .env

--- src/app/api/fcm/route.ts
+++ src/app/api/fcm/route.ts
@@ -5,8 +5,8 @@ import { google } from "googleapis";
 async function getAccessToken() {
   const auth = new google.auth.GoogleAuth({
     credentials: {
-      client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL, // Store your client email in environment variables
-      private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'), // Store your private key in environment variables
+      client_email: process.env.GOOGLE_CLIENT_EMAIL, // Store your client email in environment variables
+      private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'), // Store your private key in environment variables
     },
     scopes: ["https://www.googleapis.com/auth/firebase.messaging"],
   });
